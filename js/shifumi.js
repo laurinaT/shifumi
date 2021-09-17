e5ba5bc52e41f92344dd1ddb9ea82b4e2f66791d
laurinaT <laurinatoussaint@gmail.com>
2021-09-17 10:58:20 +0200

    Récupération du code, accueil

---
+++ js/shifumi.js
@@ -0,0 +1,7 @@
+// accueil voulez vous jouer avec moi ?
+
+if ( confirm( "Bonjour, bienvenue sur le jeu du shifumi !\n Voulez vous jouer avec moi ?" ) ) {
+    // code non nécessaire ?
+} else {
+   alert("Ooooh, vous pouvez alors fermer cette page \;\)");
+}
