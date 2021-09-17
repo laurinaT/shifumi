72cf15875b3da6652e86b89ff4f978da681af8ea
laurinaT <laurinatoussaint@gmail.com>
2021-09-17 16:25:14 +0200

    avortement + logique plus logique, quand joueur gagne

--- js/shifumi.js
+++ js/shifumi.js
@@ -31,38 +31,47 @@ function choixIA() {
     }
 }
 
-// Apprentissage des règles à l'IA
-
-var a = "pierre";
-var b = "ciseaux";
-var c = "feuille";
-var d = "Match nul"
-
-a = a + b || b + a;
-b = b + c || c + b;
-c = c + a || a + c;
-d = a + a || b + b || c + c;
-
-
-
-
-
-
-
 
 // Entrée du choix du joueur
 
-var choixJoueur = prompt("voici les règles :\n La pierre bat les ciseaux\n Les ciseaux battent la feuille \n La feuille bat la pierre \n\n Alors que choisissez vous ? Pierre, feuille ou ciseaux ?");
+var choixJoueur = prompt("voici les règles :\ Ecrivez en minuscules\n La pierre bat les ciseaux\n Les ciseaux battent la feuille \n La feuille bat la pierre \n\n Alors que choisissez vous ? Pierre, feuille ou ciseaux ?");
 
 
 // return du choix de l'IA en variable globale
 
 var choixIA = choixIA();
+
+// Apprentissage des règles à l'IA
+
+// var a = choixJoueur("pierre");
+// var b = choixJoueur("ciseaux");
+// var c = choixJoueur("feuille");
+// var d = ("Match nul");
+
+// a = a + b || b + a;
+// b = b + c || c + b;
+// c = c + a || a + c;
+// d = a + a || b + b || c + c;
+// choixIA = "pierre";
+if(choixJoueur === choixIA) {
+    alert("match nul");
+}
+
+if else(choixJoueur === "pierre" && choixIA === "ciseaux" {
+    alert("Vous avez gagné !")
+}
+
+if else(choixJoueur === "feuille" && choixIA === "pierre" {
+    alert("Vous avez gagné !")
+}
+
+if else(choixJoueur === "ciseaux" && choixIA === "feuille" {
+    alert("Vous avez gagné !")
+}
+Else{
+    alert("Vous avez perdu !")
+}
 // alert(choixIA + " bonjour"); //vérification de l'affichage
 
 
 // Comparaison choix du joueur
-
-function comparaison() {
-
-}
