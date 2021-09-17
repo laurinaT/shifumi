726f826ff15f98767eee462ef68101ac17adf547
laurinaT <laurinatoussaint@gmail.com>
2021-09-17 11:16:21 +0200

    fin accueil

--- js/shifumi.js
+++ js/shifumi.js
@@ -5,3 +5,11 @@ if ( confirm( "Bonjour, bienvenue sur le jeu du shifumi !\n Voulez vous jouer av
 } else {
    alert("Ooooh, vous pouvez alors fermer cette page \;\)");
 }
+
+// demande prénom
+var prenom = prompt("Super ! Quel est votre prénom ?");
+
+// récupération prénom --> jouer
+
+
+alert("Enchantée de vous rencontrer " + prenom + " !!!" + "\n Jouons maintenant");
