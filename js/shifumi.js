30a0fa5801be499cc8b657244bfe70f54d7f5033
laurinaT <laurinatoussaint@gmail.com>
2021-09-18 16:33:01 +0200

    Tri fonctions et inclusion du code dans une booucle afin de pouvoir rejouer

--- js/shifumi.js
+++ js/shifumi.js
@@ -1,15 +1,5 @@
-// accueil voulez-vous jouer avec moi ?
-
-if ( confirm( "Bonjour, bienvenue sur le jeu du shifumi !\nVoulez-vous jouer avec moi ?" ) ) {
-
-} else {
-   alert("Ooooh, vous pouvez alors fermer cette page \;\)");
-}
-
-// demande prénom
-var prenom = prompt("Super ! Quel est votre prénom ?");
-
-// longueur prénom
+// longueur prénom, boucle ? True, false, tant que --> while ?
+// fonction donc n'a pas besoin d'être dans la boucle
 
 function taillePrenom(prenomenvoye) {
 while (prenomenvoye.length < 2 ) {
@@ -22,10 +12,6 @@ while (prenomenvoye.length > 20 ) {
 }
 return prenomenvoye;
 }
-
-prenom = taillePrenom(prenom); 
-
-alert("Enchantée de vous rencontrer " + prenom + " !!!" + "\n Jouons maintenant.");
 
 
 // choix de l'IA
@@ -43,43 +29,53 @@ function choixIA() {
     }
 }
 
+ //----------------------------------------------------------------------------------------------------------
+    // accueil voulez-vous jouer avec moi ?
+    if ( confirm( "Bonjour ! Bienvenue sur le jeu du shifumi ! \nVoulez-vous jouer avec moi ? ")) {
+        var prenom = prompt("Super ! Quel est votre prénom ?");// demande prénom
+        prenom = taillePrenom(prenom); // réassigne la nouvelle valeur à prenom
+        var boucleJouer = true;// variable pour entrer dans boucle
+        while(boucleJouer) {
+            alert("Enchantée de vous rencontrer " + prenom + " !!!" + "\n Jouons maintenant.");
 
-// Entrée du choix du joueur
+            // Entrée du choix du joueur
+            var choixJoueur = prompt("voici les règles :\n La pierre bat les ciseaux\n Les ciseaux battent la feuille \n La feuille bat la pierre \n\n Alors que choisissez vous ? Pierre, feuille ou ciseaux ?");
 
-var choixJoueur = prompt("voici les règles :\n La pierre bat les ciseaux\n Les ciseaux battent la feuille \n La feuille bat la pierre \n\n Alors que choisissez vous ? Pierre, feuille ou ciseaux ?");
+            // majuscule ou minuscule
+            choixJoueur = choixJoueur.toLowerCase();
 
-// majuscule ou minuscule
-choixJoueur = choixJoueur.toLowerCase();
+            // return du choix de l'IA en variable globale
+            var choixIA = choixIA();
 
+            // quand match nul
+            if(choixJoueur === choixIA) {
+            alert("Désolée ! L'ordinateur a choisi " + choixIA + ". \n      Match nul !");
+            }
 
-// return du choix de l'IA en variable globale
+            // Quand joueur est gagnant
+            else if(choixJoueur === "pierre" && choixIA === "ciseaux") {
+                alert("L'ordinateur a choisi " + choixIA + ". \n        Vous avez gagné !")
+            }
 
-var choixIA = choixIA();
+            else if(choixJoueur === "feuille" && choixIA === "pierre") {
+                alert("L'ordinateur a choisi " + choixIA + ". \n        Vous avez gagné !")
+            }
 
+            else if(choixJoueur === "ciseaux" && choixIA === "feuille") {
+                alert("L'ordinateur a choisi " + choixIA + ". \n        Vous avez gagné !")
+            }
 
-// quand match nul
+            // Quand joueur perd
+            else{
+                alert("Désolée !!! L'ordinateur a choisi " + choixIA + ". \n        Vous avez perdu !")
+            }
 
-if(choixJoueur === choixIA) {
-    alert("Désolée ! L'ordinateur a choisi " + choixIA + ". Match nul !");
-}
-
-// Quand joueur est gagnant
-
-else if(choixJoueur === "pierre" && choixIA === "ciseaux") {
-    alert("L'ordinateur a choisi " + choixIA + ". Vous avez gagné !")
-}
-
-else if(choixJoueur === "feuille" && choixIA === "pierre") {
-    alert("L'ordinateur a choisi " + choixIA + ". Vous avez gagné !")
-}
-
-else if(choixJoueur === "ciseaux" && choixIA === "feuille") {
-    alert("L'ordinateur a choisi " + choixIA + ". Vous avez gagné !")
-}
-
-
-// Quand joueur perd
-
-else{
-    alert("Désolée !!! L'ordinateur a choisi " + choixIA + ". Vous avez perdu !")
-}
+            if ( !confirm( "Ce jeu vous a plus ? Vous voulez continuer ? ") ) {
+                alert("Ooooh, vous pouvez alors fermer cette page \;\)");
+                boucleJouer = false;
+            }
+            
+        }
+    } else {
+       alert("Ooooh, vous pouvez alors fermer cette page \;\)");
+    }
